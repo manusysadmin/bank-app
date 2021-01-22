e2fceb71a04364a12e29adbc32c38ed71baf0329
dzes483 <jban@sdf.org>
2021-01-22 18:49:25 +0200

    Add questionnaire

--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -3,6 +3,8 @@ import { ProductService } from '../../services/product.service';
 
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
+import { Product } from '../../model/product';
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -12,6 +14,19 @@ export class ProductListComponent {
 
   queryForm: FormGroup;
   products: any;
+
+  public ageBrackets = {
+    '0-17': 'JUNIOR',
+    '18-64': 'ADULT',
+    '65+': 'SENIOR'
+  };
+  public incomeBrackets = {
+    '0': 'NO_INCOME',
+    '1-12000': 'LOW_INCOME',
+    '12001-40000': 'MEDIUM_INCOME',
+    '40000+': 'HIGH_INCOME'
+  };
+
 
   constructor(private productService: ProductService,
               private fb: FormBuilder) {
