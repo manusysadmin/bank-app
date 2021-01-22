9b0529f84ad274b64da8c4acabb021608d04fa06
dzes483 <jban@sdf.org>
2021-01-22 20:57:48 +0200

    Update register component

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  public username: string;
+  public password: string = '';
 
-  ngOnInit(): void {
+  public message: string = '';
+  constructor(private userService: AuthService,
+              private router: Router) { }
+
+  register() {
+    this.userService.register(this.username, this.password)
+      .subscribe((resp) => {
+        console.log('Successfully registered');
+        this.message = resp.msg;
+        this.router.navigate(['login']);
+      }, (err) => {
+        console.error('Registration error', err);
+      });
   }
 
 }
