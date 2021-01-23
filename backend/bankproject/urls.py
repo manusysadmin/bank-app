b63af9de010ff275e9c34951dfac9c274e563050
dzes483 <jban@sdf.org>
2021-01-23 16:33:40 +0200

    Add backend token authentication

--- backend/bankproject/urls.py
+++ backend/bankproject/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView, RedirectView
 from rest_framework import routers
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 from bankapp import views
 from bankapp.views import *
@@ -19,6 +20,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/', include('rest_framework.urls')),
+    path('api/token/auth/', obtain_jwt_token),
+    path('api/token/refresh/', refresh_jwt_token)
 ]
 
 urlpatterns += [
