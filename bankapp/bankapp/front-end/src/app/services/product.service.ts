266985f6e21fb7ec52006a65dca142dab25f044d
dzes483 <jban@sdf.org>
2021-02-01 19:46:33 +0200

    Edit CRUD services

--- bankapp/bankapp/front-end/src/app/services/product.service.ts
+++ bankapp/bankapp/front-end/src/app/services/product.service.ts
@@ -2,41 +2,45 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Product } from '../model/product';
 
-const baseUrl = 'http://localhost:8000/api/products';
-const headers = new HttpHeaders()
-  .set('Content-Type', 'application/json');
+const BASE_URL = 'api/products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  readAll(): Observable<any>{
-    return this.httpClient.get(baseUrl, {'headers': headers});
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  // TODO: Add error handling
+
+  readAll(): Observable<Product[]> {
+    return this.http.get<Product[]>(`products`);
   }
 
-  read(): Observable<any>{
-    return this.httpClient.get(`${baseUrl}/${productName}`);
+  read(productName: string): Observable<Product> {
+    return this.http.get<Product>(`${BASE_URL}/${productName}`);
   }
 
-  create(data: any): Observable<any> {
-    return this.httpClient.post(`api/products/manage/add`, data);
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(`${BASE_URL}/manage/add`, product, this.httpOptions);
   }
 
-  update(productName: string, data: any): Observable<any> {
-    return this.httpClient.put(`${baseUrl}/manage/${productName}`, data);
+  update(product: Product, productName: string): Observable<any> {
+    return this.http.put(`${BASE_URL}/manage/${productName}`, product, this.httpOptions);
   }
 
-  delete(productName: string): Observable<any> {
-    return this.httpClient.delete(`${baseUrl}/manage/${productName}`);
+  delete(product: Product, productName: string): Observable<any> {
+    return this.http.delete(`${BASE_URL}/manage/${productName}`, this.httpOptions);
   }
 
   searchByCriteria(ageBracket: string, incomeBracket: string, student: boolean): Observable<any> {
-    return this.httpClient.get(`${baseURL}/search?ageBracket=${ageBracket}&incomeBracket=${incomeBracket}&student=${student}`,
-      {'headers': headers});
+    return this.http.get(`${BASE_URL}/search?ageBracket=${ageBracket}&incomeBracket=${incomeBracket}&student=${student}`,
+      this.httpOptions);
   }
-
 }
