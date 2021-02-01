315c43e5afa0915a191a68db6f9fd25b0926061b
dzes483 <jban@sdf.org>
2021-02-01 19:44:29 +0200

    Initialize in constructor

--- bankapp/bankapp/front-end/src/app/model/product.ts
+++ bankapp/bankapp/front-end/src/app/model/product.ts
@@ -1,8 +1,15 @@
-export interface Product {
+export class Product {
   name: string;
   age: string;
   student: boolean;
   income: string;
+
+  constructor() {
+    this.name = '';
+    this.age = '';
+    this.student = false;
+    this.income = '';
+  }
 }
 
 
