980b81e39ef126c3b0971a0dd73b220c1368d781
dzes483 <jban@sdf.org>
2021-02-05 20:05:56 +0200

    Fix spacing

--- bankapp/bankapp/front-end/src/app/components/product-add/product-add.component.ts
+++ bankapp/bankapp/front-end/src/app/components/product-add/product-add.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Product } from '../../model/product';
 
 @Component({
