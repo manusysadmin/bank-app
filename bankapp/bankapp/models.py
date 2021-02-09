a0251041a64bde3a0e2163a93abc7c268b28d375
dzes483 <jban@sdf.org>
2021-02-09 20:49:56 +0200

    Add multi-select fields

--- bankapp/bankapp/models.py
+++ bankapp/bankapp/models.py
@@ -16,36 +16,12 @@ INCOME_CHOICES = [
     ('HIGH_INCOME', '40000+')
 ]
 
-class AgeBracket(models.Model):
-    AGE_CHOICES = [
-        ('JUNIOR', '0-17'),
-        ('ADULT', '18-64'),
-        ('SENIOR', '64+')
-    ]
-    age = models.CharField(max_length=300, choices=AGE_CHOICES)
-
-    def __str__(self):
-        return f"{self.age} ({self.get_age_display()})"
-
-
-class IncomeBracket(models.Model):
-    INCOME_BRACKET_CHOICES = [
-        ('NO_INCOME', '0'),
-        ('LOW_INCOME', '1-12000'),
-        ('MEDIUM_INCOME', '12001-40000'),
-        ('HIGH_INCOME', '40000+')
-    ]
-    income = models.CharField(max_length=300, choices=INCOME_BRACKET_CHOICES)
-
-    def __str__(self):
-        return f"{self.income} ({self.get_income_display()})"
-
 
 class Product(models.Model):
     name = models.CharField(blank=False, max_length=120, unique=True)
-    age = MultiSelectField(choices=AGE_CHOICES, default='')
+    age = MultiSelectField(choices=AGE_CHOICES)
     student = models.BooleanField(blank=False)
-    income = MultiSelectField(choices=INCOME_CHOICES, default='')
+    income = MultiSelectField(choices=INCOME_CHOICES)
     slug = models.SlugField(default='', editable=False, max_length=120)
 
     def save(self, *args, **kwargs):
