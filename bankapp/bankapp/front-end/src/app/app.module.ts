2f3b5f6ccabaedccf68860815fd6440b44efe2a7
dzes483 <jban@sdf.org>
2021-02-10 20:11:30 +0200

    Code cleanup

--- bankapp/bankapp/front-end/src/app/app.module.ts
+++ bankapp/bankapp/front-end/src/app/app.module.ts
@@ -4,22 +4,15 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
 import { ProductAddComponent } from './components/product-add/product-add.component';
 import { ProductService } from './services/product.service';
-import { AuthService } from './services/auth.service';
-import { AuthInterceptor } from './interceptors/auth-interceptor';
-import { AuthGuard } from './guards/auth.guard';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     ProductAddComponent,
     ProductListComponent,
-    RegisterComponent
   ],
   imports: [
     BrowserModule,
@@ -29,13 +22,6 @@ import { RegisterComponent } from './components/register/register.component';
   ],
   providers: [
     ProductService,
-    AuthService,
-    AuthGuard,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true,
-    }
   ],
   bootstrap: [AppComponent]
 })
