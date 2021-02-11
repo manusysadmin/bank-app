e738c343fc0e3490a528e5c043b63f8d280d6cdc
dzes483 <jban@sdf.org>
2021-02-11 20:06:14 +0200

    Add search URL

--- bankapp/bankproject/urls.py
+++ bankapp/bankproject/urls.py
@@ -20,10 +20,10 @@ urlpatterns = [
     path('api/', include('rest_framework.urls')),
     path('api/login/', obtain_jwt_token),
     path('api/manage/add', views.ProductCreateViewSet.as_view()),
-    path('api/products', views.ProductListViewSet.as_view()),
+    path('api/products/search', views.ProductListViewSet.as_view()),
     path('api/refresh-token/', refresh_jwt_token)
 ]
 
-urlpatterns += [
-    re_path(r'(?P<path>.*)', FrontEndRenderView.as_view())
-]
+# urlpatterns += [
+#     re_path(r'(?P<path>.*)', FrontEndRenderView.as_view())
+# ]
