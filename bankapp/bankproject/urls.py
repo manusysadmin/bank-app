8aacdf954cb2c082b7c52d229f248b33830633ef
dzes483 <jban@sdf.org>
2021-02-12 19:07:04 +0200

    Remove unused imports, add api/manage url

--- bankapp/bankproject/urls.py
+++ bankapp/bankproject/urls.py
@@ -1,17 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView, RedirectView
+from django.urls import path, include
 from rest_framework import routers
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 from bankapp import views
-from bankapp.views import *
 
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
-router.register(r'manage', views.ProductDetailViewSet)
 
 
 urlpatterns = [
@@ -19,7 +16,9 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/', include('rest_framework.urls')),
     path('api/login/', obtain_jwt_token),
+    path('api/manage', views.ProductListViewSet.as_view()),
     path('api/manage/add', views.ProductCreateViewSet.as_view()),
+    path('api/products', views.ProductListViewSet.as_view()),
     path('api/products/search', views.ProductListViewSet.as_view()),
     path('api/refresh-token/', refresh_jwt_token)
 ]
