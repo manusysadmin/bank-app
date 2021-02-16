1e925ce7c8b0556072930e3d1508178a2c1b708f
dzes483 <jban@sdf.org>
2021-02-16 16:24:03 +0200

    Create product edit form, update() method

--- bankapp/bankapp/front-end/src/app/components/product-detail/product-detail.component.ts
+++ bankapp/bankapp/front-end/src/app/components/product-detail/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,39 +10,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  productName!: string;
+  productAge!: Array<string>;
+  productEditForm: FormGroup;
+  productSlug!: string;
+  isSubmitted = false;
+  isUpdateFailed = false;
+  errorMessage = '';
+  public ageBrackets = {
+    '0-17': 'JUNIOR',
+    '18-64': 'ADULT',
+    '65+': 'SENIOR'
+  };
+  public incomeBrackets = {
+    '0': 'NO_INCOME',
+    '1-12000': 'LOW_INCOME',
+    '12001-40000': 'MEDIUM_INCOME',
+    '40000+': 'HIGH_INCOME'
+  };
+
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute,
+              private fb: FormBuilder) {
+    this.productEditForm = this.fb.group({
+      name: [null, [Validators.required, Validators.minLength(3)]],
+      age: [null, Validators.required],
+      student: [false, Validators.required],
+      income: [null, Validators.required]
+    });
+  }
 
   ngOnInit(): void {
+    this.isSubmitted = false;
+    this.isUpdateFailed = false;
+    this.getProduct(this.route.snapshot.paramMap.get('productSlug') || '');
   }
 
-  slugify(productName: string): void {
-    const slug = this.slugifyPipe.transform(productName);
-  }
-
-  getProduct(): void {
-    const productSlug = this.route.snapshot.params.productSlug;
+  getProduct(productSlug: string): any {
     this.productService.get(productSlug).subscribe(
       response => {
+        productSlug = response.slug;
+        this.productName = response.name;
+        this.productAge = response.age;
         console.log(response);
+        console.log(productSlug);
+        console.log(this.productName);
+        this.productEditForm.setValue({
+          name: response.name,
+          age: response.age,
+          student: response.student,
+          income: response.income
+        });
+      },
+      error => {
+        console.log(error);
       }
     );
+    return productSlug;
   }
 
-  // getProduct(productSlug: string): void {
-  //   this.productService.get(productSlug).subscribe(
-  //     response => {
-  //       console.log(response);
-  //       this.productEditForm.patchValue({
-  //         name: response.results.name,
-  //         age: response.results.age,
-  //         student: response.results.student,
-  //         income: response.results.income
-  //       });
-  //     }, error => {
-  //       console.log(error);
-  //     }
-  //   );
-  // }
+  updateProduct(): void {
+    this.productService.update(this.productSlug, this.productEditForm.value)
+      .subscribe(
+        (response: any) => {
+          console.log(response);
+          this.isSubmitted = true;
+        },
+        (error: any) => {
+          this.errorMessage = error.error.message;
+          this.isUpdateFailed = true;
+          console.log(error);
+        }
+      );
+  }
 
-
+  // Getters
+  get name(): any {
+    return this.productEditForm.get('name');
+  }
+  get age(): any {
+    return this.productEditForm.get('age');
+  }
+  get income(): any {
+    return this.productEditForm.get('income');
+  }
+  get student(): any {
+    return this.productEditForm.get('student');
+  }
 }
