ed3fcb65258a1e2e23d0c6228f49d7b5e6bd8913
dzes483 <jban@sdf.org>
2021-02-16 16:23:20 +0200

    Add detail component to URLs

--- bankapp/bankapp/front-end/src/app/app-routing.module.ts
+++ bankapp/bankapp/front-end/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductAddComponent } from './components/product-add/product-add.component';
 import { ProductManageComponent } from './components/product-manage/product-manage.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
 
 const routes: Routes = [
   { path: 'api/products', component: ProductListComponent },
   { path: 'api/manage/add', component: ProductAddComponent },
-  { path: 'api/manage/:productSlug', component: ProductManageComponent},
-  { path: 'api/manage', component: ProductManageComponent},
+  { path: 'api/manage/:productSlug', component: ProductDetailComponent },
+  { path: 'api/manage', component: ProductManageComponent },
 ];
 
 @NgModule({
