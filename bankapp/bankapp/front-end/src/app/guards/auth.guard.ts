6ec9c738b85786a06af4add3f5ef181a2bf13a6b
dzes483 <jban@sdf.org>
2021-02-18 21:04:11 +0200

    Create auth guard

---
+++ bankapp/bankapp/front-end/src/app/guards/auth.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router,
+              private authService: AuthService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const currentUser = this.authService.currentUserValue;
+    if (currentUser) {
+      // authorized, so return true
+      return true;
+    }
+
+    // not logged in, so redirect to login page with the return URL
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url}});
+    return false;
+  }
+
+
+}
