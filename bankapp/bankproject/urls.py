a86dce871aae68ee8119a8b27ba480b973206b8e
dzes483 <jban@sdf.org>
2021-02-18 14:44:13 +0200

    Fix URL ordering

--- bankapp/bankproject/urls.py
+++ bankapp/bankproject/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 from bankapp import views
 
@@ -15,13 +14,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/', include('rest_framework.urls')),
-    path('api/login/', obtain_jwt_token),
+    path('api/manage/add', views.ProductCreateViewSet.as_view()),
     path('api/manage', views.ProductListViewSet.as_view()),
     path('api/manage/<slug:slug>', views.ProductDetailViewSet.as_view()),
-    path('api/manage/add', views.ProductCreateViewSet.as_view()),
     path('api/products', views.ProductListViewSet.as_view()),
     path('api/products/search', views.ProductListViewSet.as_view()),
-    path('api/refresh-token/', refresh_jwt_token)
 ]
 
 # urlpatterns += [
