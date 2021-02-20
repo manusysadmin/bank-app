b5ef2cf3d24a5d2c4627b27cb743125b413fd6d9
dzes483 <jban@sdf.org>
2021-02-20 18:03:06 +0200

    Create register component

---
+++ bankapp/bankapp/front-end/src/app/components/register/register.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  register(username: string, password1: string, password2: string) {
+    // TODO: register in service
+  }
+}
