759c381b74178dc143f2aba1274574cd148f6eeb
dzes483 <jban@sdf.org>
2021-02-20 18:05:20 +0200

    Implement token

--- bankapp/bankapp/front-end/src/app/components/login/login.component.ts
+++ bankapp/bankapp/front-end/src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,8 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup;
 
   constructor(private authService: AuthService,
-              private fb: FormBuilder) {
+              private fb: FormBuilder,
+              private router: Router) {
     this.loginForm = this.fb.group({
       username: [null, Validators.required],
       password: [null, Validators.required]
@@ -26,11 +28,14 @@ export class LoginComponent implements OnInit {
     this.authService.login(this.loginForm.value).subscribe(
       (response: any) => {
         console.log(response);
+        if (response.access_token) {
+          this.authService.storeInLocalStorage('token', response.access_token);
+          this.router.navigate(['api/products']).then(r => console.log(r));
+        }
       },
       (error: any) => {
         console.log(error);
-      }
-    );
+      });
   }
 
 }
--- bankapp/bankapp/front-end/src/app/services/auth.service.ts
+++ bankapp/bankapp/front-end/src/app/services/auth.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import * as moment from 'moment';
 
 const BASE_URL = 'http://localhost:8000/api';
-
+const TOKEN_KEY = 'auth-token';
+const USER_KEY = 'auth-user';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +14,23 @@ export class AuthService {
 
   constructor(private http: HttpClient) {}
 
-  login(credentials: any): any {
-    return this.http.post(`${BASE_URL}/login/`, credentials);
+  login(credentials: any): Observable<any> {
+    return this.http.post(`${BASE_URL}/login`, credentials);
+  }
+
+  getUserDetails(): any {
+    return localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo') || '') : null;
+  }
+
+  storeInLocalStorage(variableName: any, data: any): void {
+    localStorage.setItem(variableName, data);
+  }
+
+  getToken(): any {
+    return localStorage.getItem('token');
+  }
+
+  clearStorage(): void {
+    localStorage.clear();
   }
 }
