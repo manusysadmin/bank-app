63247f312329c801fb746d5b6593ad54db2779a9
dzes483 <jban@sdf.org>
2021-02-20 18:03:55 +0200

    Add token urls

--- bankapp/bankproject/urls.py
+++ bankapp/bankproject/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 from bankapp import views
 
 
@@ -11,14 +11,16 @@ router.register(r'groups', views.GroupViewSet)
 
 
 urlpatterns = [
+    path('api/login', obtain_jwt_token),
+    path('api/refresh_jwt_token', refresh_jwt_token),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/', include('rest_framework.urls')),
     path('api/manage/add', views.ProductCreateViewSet.as_view()),
-    path('api/manage', views.ProductListViewSet.as_view()),
     path('api/manage/<slug:slug>', views.ProductDetailViewSet.as_view()),
-    path('api/products', views.ProductListViewSet.as_view()),
+    path('api/manage', views.ProductListViewSet.as_view()),
     path('api/products/search', views.ProductListViewSet.as_view()),
+    path('api/products', views.ProductListViewSet.as_view()),
 ]
 
 # urlpatterns += [
