5ec7434e94f80edcf4a3070c51071bd99e5916f7
dzes483 <jban@sdf.org>
2021-02-21 16:12:10 +0200

    Add register URL

--- bankapp/bankapp/front-end/src/app/app-routing.module.ts
+++ bankapp/bankapp/front-end/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { ProductAddComponent } from './components/product-add/product-add.component';
 import { ProductManageComponent } from './components/product-manage/product-manage.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
-import {LoginComponent} from './components/login/login.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 const routes: Routes = [
@@ -13,7 +14,8 @@ const routes: Routes = [
   { path: 'api/manage/add', component: ProductAddComponent },
   { path: 'api/manage/:productSlug', component: ProductDetailComponent },
   { path: 'api/manage', component: ProductManageComponent },
-  { path: 'api/login', component: LoginComponent }
+  { path: 'api/login', component: LoginComponent },
+  { path: 'api/register', component: RegisterComponent }
 ];
 
 @NgModule({
