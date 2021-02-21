229d8bc402582b6d12ce253ea4b0b6f8a13390ea
dzes483 <jban@sdf.org>
2021-02-21 16:11:57 +0200

    Implement auth guards

--- bankapp/bankapp/front-end/src/app/app.component.ts
+++ bankapp/bankapp/front-end/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from './services/token-storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'front-end';
+  isLoggedIn = false;
+  username?: string;
+
+  constructor(private tokenStorageService: TokenStorageService) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+
+      this.username = user.username;
+    }
+  }
+
+  logout(): void {
+    this.tokenStorageService.logout();
+    window.location.reload();
+  }
 }
--- bankapp/bankapp/front-end/src/app/helpers/auth.guard.ts
+++ bankapp/bankapp/front-end/src/app/helpers/auth.guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
-import { AuthService } from '../services/auth.service';
+import { TokenStorageService } from '../services/token-storage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +10,13 @@ import { AuthService } from '../services/auth.service';
 export class AuthGuard implements CanActivate {
 
   constructor(private router: Router,
-              private authService: AuthService) {}
+              private tokenStorage: TokenStorageService) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (this.authService.getToken()) {
+    const user = this.tokenStorage.getUser();
+    if (this.tokenStorage.getToken()) {
       return true;
     } else {
       // not logged in, so redirect to login page with the return URL
