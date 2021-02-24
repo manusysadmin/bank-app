49bd6b429c3acc195c3a1568a7869b25556bf9cc
dzes483 <jban@sdf.org>
2021-02-24 20:45:03 +0200

    Create user list component

---
+++ bankapp/bankapp/front-end/src/app/components/user-list/user-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../services/user.service';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.scss']
+})
+export class UserListComponent implements OnInit {
+
+  users: any;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.userService.getAll()
+      .subscribe(
+        (response: any) => {
+          this.users = response.results;
+          console.log(response.results);
+        },
+        (error: any) => {
+          console.log(error);
+        }
+      );
+  }
+}
