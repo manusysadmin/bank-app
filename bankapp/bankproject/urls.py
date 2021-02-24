a2cd0bac9d45c53fc597d0a1a6eed216fa2f692a
dzes483 <jban@sdf.org>
2021-02-24 20:46:16 +0200

    Add URLs for groups and users

--- bankapp/bankproject/urls.py
+++ bankapp/bankproject/urls.py
@@ -1,26 +1,25 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from bankapp import views
-
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
 
 
 urlpatterns = [
     path('api/login', TokenObtainPairView.as_view()),
     path('api/refresh_jwt_token', TokenRefreshView.as_view()),
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
     path('api/', include('rest_framework.urls')),
     path('api/manage/add', views.ProductCreateViewSet.as_view()),
     path('api/manage/<slug:slug>', views.ProductDetailViewSet.as_view()),
+    path('api/manage/users/<pk>', views.UserEditView.as_view()),
+    path('api/manage/groups/<pk>', views.GroupEditView.as_view()),
     path('api/manage', views.ProductListViewSet.as_view()),
     path('api/products/search', views.ProductListViewSet.as_view()),
     path('api/products', views.ProductListViewSet.as_view()),
+    path('api/register', views.UserCreateView.as_view()),
+    path('api/users', views.UserListView.as_view()),
+    path('api/groups', views.GroupListView.as_view()),
+    path('api/groups/add', views.GroupCreateView.as_view())
 ]
 
 # urlpatterns += [
