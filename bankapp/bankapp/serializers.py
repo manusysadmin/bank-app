d4ec6206f58b9a2979ec3a4ed6c6a3c97ecddd4e
dzes483 <jban@sdf.org>
2021-02-25 19:18:40 +0200

    Create permission and group serializers

--- bankapp/bankapp/serializers.py
+++ bankapp/bankapp/serializers.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User, Group, Permission
 from rest_framework import serializers
 
 from bankapp.models import Product
@@ -15,13 +15,39 @@ class ProductSerializer(serializers.ModelSerializer):
         }
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'groups']
-
-
 class GroupSerializer(serializers.ModelSerializer):
+
+    def to_representation(self, instance):
+        return instance.name
+
     class Meta:
         model = Group
-        fields = ['url', 'name', 'permissions']
+        fields = ['name']
+
+
+class PermissionSerializer(serializers.ModelSerializer):
+
+    def to_representation(self, instance):
+        return instance.name
+
+    class Meta:
+        model = Permission
+        fields = ['name']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    groups = GroupSerializer(read_only=True, many=True)
+    user_permissions = PermissionSerializer(read_only=True, many=True)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        return user
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'user_permissions', 'id', 'groups']
+
