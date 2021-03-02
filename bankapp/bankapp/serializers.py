a93f3d422759814051f9c007b7141f06004c02d5
dzes483 <jban@sdf.org>
2021-03-02 19:29:25 +0200

    Display last login

--- bankapp/bankapp/serializers.py
+++ bankapp/bankapp/serializers.py
@@ -10,6 +10,7 @@ class TokenSerializer(TokenObtainPairSerializer):
         token = super().get_token(user)
 
         token['role'] = user.role
+        token['username'] = user.username
 
         return token
 
@@ -27,6 +28,13 @@ class ProductSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    last_login = serializers.SerializerMethodField()
+
+    def get_last_login(self, user):
+        last_login = CustomUser.objects.get(username=user).last_login
+        if last_login is not None:
+            return last_login.strftime('%y-%m-%d %a %H:%M:%S')
+        return None
 
     def create(self, validated_data):
         user = CustomUser.objects.create_user(
@@ -37,4 +45,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'password', 'role']
+        fields = ['id', 'username', 'password', 'role', 'last_login']
