53f53c30991c9f6fe7129fca390c81acd2b9d4e1
dzes483 <jban@sdf.org>
2021-03-15 20:09:59 +0200

    Remove unused import

--- back-end/bankproject/urls.py
+++ back-end/bankproject/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from bankapp import views
 from bankapp.views import TokenView
