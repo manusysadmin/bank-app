16fc6f04c882acb9ca49a4151905d77880a7b025
dzes483 <jban@sdf.org>
2021-03-15 20:11:00 +0200

    Add test for backend user creation

--- back-end/bankapp/tests.py
+++ back-end/bankapp/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from .models import CustomUser
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, force_authenticate
+
+class UserCreateTestCase(APITestCase):
+
+    def test_usercreate(self):
+        data = {"username": "testcase", "password": "testpassword"}
+
+        response = self.client.post("/api/register", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
 
 # Create your tests here.
