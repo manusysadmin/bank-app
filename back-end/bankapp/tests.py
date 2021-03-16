3c1fad9add3b036a8c537136d5d658f773474167
dzes483 <jban@sdf.org>
2021-03-16 21:20:39 +0200

    Create auth test case

--- back-end/bankapp/tests.py
+++ back-end/bankapp/tests.py
@@ -1,7 +1,26 @@
 from .models import CustomUser
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase, force_authenticate
+from rest_framework.test import APITestCase, force_authenticate, APIClient
+
+
+class AuthTestCase(APITestCase):
+
+    def setUp(self):
+        self.username = "test-user"
+        self.password = "testpassword"
+        self.user = CustomUser.objects.create_user(username=self.username,
+                                                   password=self.password)
+        self.user.save()
+
+    def test_login_and_jwt(self):
+        response = self.client.post("/api/login", {'username': 'test-user',
+                                                   'password': 'testpassword'}, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue('access' in response.data)
+        self.assertTrue('refresh' in response.data)
+
 
 class UserCreateTestCase(APITestCase):
 
