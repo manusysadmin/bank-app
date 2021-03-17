566068487c584997bbf80b571da5ddabebf0c9e2
dzes483 <jban@sdf.org>
2021-03-17 21:15:24 +0200

    Reorder URLs

--- back-end/bankproject/urls.py
+++ back-end/bankproject/urls.py
@@ -2,15 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from bankapp import views
-from bankapp.views import TokenView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/login', TokenView.as_view()),
+    path('api/login', views.TokenView.as_view()),
     path('api/register', views.UserCreateView.as_view()),
     path('api/refresh_jwt_token', TokenRefreshView.as_view()),
-    path('api/manage/products/<slug:slug>', views.ProductDetailView.as_view()),
     path('api/manage/products/add', views.ProductCreateView.as_view()),
+    path('api/manage/products/<slug:slug>', views.ProductDetailView.as_view()),
     path('api/manage/products', views.ProductListView.as_view()),
     path('api/manage/users/<pk>', views.UserEditView.as_view()),
     path('api/manage/users', views.UserListView.as_view()),
