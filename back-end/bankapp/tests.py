d525936c2845f6a0ecd53d814219dab5add046bf
dzes483 <jban@sdf.org>
2021-03-18 21:15:17 +0200

    Add product tests

--- back-end/bankapp/tests.py
+++ back-end/bankapp/tests.py
@@ -4,31 +4,41 @@ from rest_framework import status
 from rest_framework.test import APITestCase, force_authenticate, APIClient
 
 
-class AuthTestCase(APITestCase):
+
+class ProductCreateTestCase(APITestCase):
 
     def setUp(self):
-        self.username = "test-user"
-        self.password = "testpassword"
-        self.user = CustomUser.objects.create_user(username=self.username,
-                                                   password=self.password)
+        data = {"username": "test-admin", "password": "testpassword"}
+        self.superuser = CustomUser.objects.create_superuser(username="test-admin",
+                                                             password="testpassword")
+        self.superuser.save()
+
+        self.user = CustomUser.objects.create_user(username='test-user', password='testpassword')
         self.user.save()
 
-    def test_login_and_jwt(self):
-        response = self.client.post("/api/login", {'username': 'test-user',
-                                                   'password': 'testpassword'}, format='json')
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue('access' in response.data)
-        self.assertTrue('refresh' in response.data)
-
-
-class UserCreateTestCase(APITestCase):
-
-    def test_usercreate(self):
-        data = {"username": "testcase", "password": "testpassword"}
-
-        response = self.client.post("/api/register", data)
+    def test_productcreate(self):
+        client = APIClient()
+        login_response = self.client.post("/api/login", {'username': 'test-admin',
+                                                         'password': 'testpassword'}, format='json')
+        token = login_response.data['access']
+        client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+        data = {"name": "testproduct",
+                "age": ['JUNIOR'],
+                "student": False,
+                "income": ["NO_INCOME"]}
+        response = client.post("/api/manage/products/add", data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Product.objects.count(), 1)
 
-
-# Create your tests here.
+    def test_productcreate_unauthorized(self):
+        client = APIClient()
+        login_response = self.client.post("/api/login", {'username': 'test-user',
+                                                         'password': 'testpassword'}, format='json')
+        client.credentials(HTTP_AUTHORIZATION='Bearer ' + login_response.data['access'])
+        data = {"name": "testproduct",
+                "age": ['JUNIOR'],
+                "student": False,
+                "income": ["NO_INCOME"]}
+        response = client.post("/api/manage/products/add", data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(Product.objects.count(), 0)
